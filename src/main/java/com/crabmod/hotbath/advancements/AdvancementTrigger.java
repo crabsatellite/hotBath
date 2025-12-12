dd5ae8b3fe3452d63f0b1633a4756ad8cab6511f
Alex Li <islicy@outlook.com>
2025-12-12 22:38:45 +0900

    Refactor AdvancementTrigger and update event registration; remove unused advancements and recipes

--- src/main/java/com/crabmod/hotbath/advancements/AdvancementTrigger.java
+++ src/main/java/com/crabmod/hotbath/advancements/AdvancementTrigger.java
@@ -1,38 +1,76 @@
 package com.crabmod.hotbath.advancements;
 
 import com.google.gson.JsonObject;
-import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
-import net.minecraft.advancements.critereon.ContextAwarePredicate;
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.advancements.CriterionTrigger;
+import net.minecraft.advancements.CriterionTriggerInstance;
 import net.minecraft.advancements.critereon.DeserializationContext;
-import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
+import net.minecraft.advancements.critereon.SerializationContext;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
 
-@SuppressWarnings("removal")
-public class AdvancementTrigger extends SimpleCriterionTrigger<AdvancementTrigger.Instance> {
+public class AdvancementTrigger implements CriterionTrigger<AdvancementTrigger.Instance> {
+  private final ResourceLocation ID;
+  private final List<Listener<Instance>> listeners = new ArrayList<>();
+
+  @SuppressWarnings("removal")
+  public AdvancementTrigger(String modName, String advancementName) {
+    this.ID = new ResourceLocation(modName, advancementName);
+  }
+
+  @Override
+  public @NotNull ResourceLocation getId() {
+    return ID;
+  }
+
+  @Override
+  public void addPlayerListener(
+          @NotNull PlayerAdvancements playerAdvancements, @NotNull Listener<Instance> listener) {
+    this.listeners.add(listener);
+  }
+
+  @Override
+  public void removePlayerListener(
+          @NotNull PlayerAdvancements playerAdvancements, @NotNull Listener<Instance> listener) {
+    this.listeners.remove(listener);
+  }
+
+  @Override
+  public void removePlayerListeners(@NotNull PlayerAdvancements playerAdvancements) {
+    this.listeners.clear();
+  }
+
+  @Override
+  public @NotNull Instance createInstance(
+          @NotNull JsonObject json, @NotNull DeserializationContext context) {
+    return new Instance(ID);
+  }
+
+  public void trigger(ServerPlayer player) {
+    List<Listener<Instance>> listenersCopy = new ArrayList<>(this.listeners);
+    listenersCopy.forEach(listener -> listener.run(player.getAdvancements()));
+  }
+
+  public static class Instance implements CriterionTriggerInstance {
     private final ResourceLocation id;
 
-    public AdvancementTrigger(String modName, String advancementName) {
-        this.id = new ResourceLocation(modName, advancementName);
+    public Instance(ResourceLocation id) {
+      this.id = id;
     }
 
     @Override
-    public ResourceLocation getId() {
-        return this.id;
+    public @NotNull ResourceLocation getCriterion() {
+      return id;
     }
 
+    // Implement the necessary methods for serialization if needed
     @Override
-    protected Instance createInstance(JsonObject json, ContextAwarePredicate player, DeserializationContext context) {
-        return new Instance(this.id, player);
+    public @NotNull JsonObject serializeToJson(@NotNull SerializationContext context) {
+      // Return a JSON object representing the conditions of this instance
+      return new JsonObject();
     }
-
-    public void trigger(ServerPlayer player) {
-        this.trigger(player, instance -> true);
-    }
-
-    public static class Instance extends AbstractCriterionTriggerInstance {
-        public Instance(ResourceLocation id, ContextAwarePredicate player) {
-            super(id, player);
-        }
-    }
+  }
 }
--- src/main/java/com/crabmod/hotbath/registers/ExtraEventsRegister.java
+++ src/main/java/com/crabmod/hotbath/registers/ExtraEventsRegister.java
@@ -4,18 +4,20 @@ import com.crabmod.hotbath.HotBath;
 import com.crabmod.hotbath.advancements.AdvancementTrigger;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@EventBusSubscriber(modid = HotBath.MOD_ID)
+@Mod.EventBusSubscriber(modid = HotBath.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ExtraEventsRegister {
 
     @SubscribeEvent
-    public static void registerAdvancementTrigger(RegisterEvent event) {
-        CriteriaTriggers.register(new AdvancementTrigger("hotbath", "foot_health"));
-        CriteriaTriggers.register(new AdvancementTrigger("hotbath", "milk_skin"));
-        CriteriaTriggers.register(new AdvancementTrigger("hotbath", "chronic_invalid"));
-        CriteriaTriggers.register(new AdvancementTrigger("hotbath", "rose_body_fragrance"));
+    public static void registerAdvancementTrigger(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            CriteriaTriggers.register(new AdvancementTrigger("hotbath", "foot_health"));
+            CriteriaTriggers.register(new AdvancementTrigger("hotbath", "milk_skin"));
+            CriteriaTriggers.register(new AdvancementTrigger("hotbath", "chronic_invalid"));
+            CriteriaTriggers.register(new AdvancementTrigger("hotbath", "rose_body_fragrance"));
+        });
     }
 }
 
